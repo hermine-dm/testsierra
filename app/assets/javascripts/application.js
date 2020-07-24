e2b102ccb31ad7f189555be2e75f2ad06ee05b23
hermine-dm <herdementh@gmail.com>
2020-07-24 14:57:04 +0200

    first

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,39 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require jquery-3.2.1.min
+       
+//= require popper.min
+//= require bootstrap.min
+
+//= require activestorage
+//= require turbolinks
+
+//=require revolution/js/jquery.themepunch.tools.min
+//=require revolution/js/jquery.themepunch.revolution.min
+
+//= require revolution/js/extensions/revolution.extension.actions.min
+//= require revolution/js/extensions/revolution.extension.video.min
+
+//= require revolution/js/extensions/revolution.extension.layeranimation.min
+//= require revolution/js/extensions/revolution.extension.navigation.min
+//= require revolution/js/extensions/revolution.extension.slideanims.min
+
+//= require counterup/jquery.waypoints.min
+//= require counterup/jquery.counterup.min 
+//= require counterup/apear
+//= require counterup/countto
+//= require owl-carousel/owl.carousel.min
+//= require magnify-popup/jquery.magnific-popup.min
+//= require theme
+//= require_tree .
